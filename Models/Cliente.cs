ef303cb8494156c2172a86d5fb178e4c29aea26f
JULIETA ROMANO <49703405@ort>
2026-05-07 08:21:50 -0300

    creacion de la clase cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,5 +2,8 @@ namespace Ejercicio5.Models;
 
 public class Cliente 
 {
-    
+    public bool esApto (int edad, double ingresos , bool estaTrabajando ,  double montoPrestamo , bool deudasPrevias)
+    {
+        
+    }
 }
